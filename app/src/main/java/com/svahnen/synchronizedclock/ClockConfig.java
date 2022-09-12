5fdba59bc83d59f7bf2159b81077acaba8c2c962
Svahnen <svahn.jonny@gmail.com>
2022-09-12 13:07:33 +0200

    Change text from java

---
+++ app/src/main/java/com/svahnen/synchronizedclock/ClockConfig.java
@@ -0,0 +1,12 @@
+package com.svahnen.synchronizedclock;
+
+import android.widget.TextView;
+
+public class ClockConfig {
+    private TextView clock;
+
+    public ClockConfig(TextView clock) {
+        this.clock = clock;
+        clock.setText("some text");
+    }
+}
--- app/src/main/java/com/svahnen/synchronizedclock/MainActivity.java
+++ app/src/main/java/com/svahnen/synchronizedclock/MainActivity.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import android.widget.TextView;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView clock = findViewById(R.id.clock);
+        ClockConfig clockConfig = new ClockConfig(clock);
     }
 }
