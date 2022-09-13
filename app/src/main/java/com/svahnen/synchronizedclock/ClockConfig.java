715e8e85322d21a26614c8f644fa4356f329a88c
BuildTools <unconfigured@null.spigotmc.org>
2022-09-13 11:12:29 +0200

    Semi broken

--- app/src/main/java/com/svahnen/synchronizedclock/ClockConfig.java
+++ app/src/main/java/com/svahnen/synchronizedclock/ClockConfig.java
@@ -3,52 +3,40 @@ package com.svahnen.synchronizedclock;
 import android.widget.TextView;
 import org.apache.commons.net.ntp.NTPUDPClient;
 import org.apache.commons.net.ntp.TimeInfo;
-import org.apache.commons.net.time.TimeTCPClient;
 
 import java.io.IOException;
 import java.net.InetAddress;
-import java.net.UnknownHostException;
 import java.util.Date;
 
 public class ClockConfig  {
-    private TextView clock;
+
     public static final String TIME_SERVER = "time-a.nist.gov";
 
     public ClockConfig(TextView clock) throws IOException {
-        this.clock = clock;
-        clock.setText("Some text");
-        //System.out.println(getCurrentNetworkTime());
 
-        Thread thread = new Thread(new Runnable() {
+        clock.setText("Loading...");
 
-            @Override
-            public void run() {
-                try  {
-                    //Your code goes here
-                    System.out.println(getCurrentNetworkTime());
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
+        Thread thread = new Thread(() -> {
+            try  {
+                //System.out.println("Time is: " + getCurrentNetworkTime());
+                clock.setText(getCurrentNetworkTime().toString());
+                //Thread.currentThread().interrupt();
+            } catch (Exception e) {
+                e.printStackTrace();
             }
         });
 
         thread.start();
 
-
-
     }
 
-    public static long getCurrentNetworkTime() throws IOException {
+    public static Date getCurrentNetworkTime() throws IOException {
         NTPUDPClient timeClient = new NTPUDPClient();
         InetAddress inetAddress = InetAddress.getByName(TIME_SERVER);
         TimeInfo timeInfo = timeClient.getTime(inetAddress);
+        long returnTime = timeInfo.getMessage().getTransmitTimeStamp().getTime();
         //long returnTime = timeInfo.getReturnTime();   //local device time
-        long returnTime = timeInfo.getMessage().getTransmitTimeStamp().getTime();   //server time
 
-        Date time = new Date(returnTime);
-
-        System.out.println("Time from " + TIME_SERVER + ": " + time);
-
-        return returnTime;
+        return new Date(returnTime);
     }
 }
